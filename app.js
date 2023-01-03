7c9f6fa32ba0c376b48dc78f1ec169286d6b426a
imjayapple <thisisfakeemail@gmai.com>
2023-01-02 19:02:48 -0800

    Beginning of formula calculations, setting initial variables

---
+++ app.js
@@ -0,0 +1,16 @@
+//First start off by adding a variable to represent your age
+
+const myAge = 50;
+
+//The first two years, set to early years
+
+let earlyYears = 2;
+
+//Conversion formula from dog years to human years?
+
+earlyYears *= 10.5;
+
+//
+
+let laterYears = myAge - 2;
+
