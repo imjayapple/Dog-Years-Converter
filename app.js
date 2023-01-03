3849664aa7c7a236a65acff9d1cec153911452d7
imjayapple <thisisfakeemail@gmai.com>
2023-01-02 19:36:03 -0800

    Added remaining variables and formulas to execute and obtain dog years to human years

--- app.js
+++ app.js
@@ -2,15 +2,30 @@
 
 const myAge = 50;
 
-//The first two years, set to early years
+//Set the early years value to 2, this represents the early years of the dog
 
 let earlyYears = 2;
 
-//Conversion formula from dog years to human years?
+//Conversion formula from dog years to human years in the early years
 
 earlyYears *= 10.5;
 
-//
+//Determine the value of the later yeras of the dog by subtracting 2 from your current age
 
 let laterYears = myAge - 2;
 
+//Conversion formula from dog years to human years in the early years
+
+laterYears *= 4;
+
+//Formula for total dog years, add both variables together
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+//Convert name provided to myName variable to lowercase
+
+let myName = 'Jay'.toLowerCase()
+
+//Log to the console a statement using all of the variables to obtain dog years from human years, and display both
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
